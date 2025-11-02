b6b6e4c6da6a7dc37e62df2839aa19727b6c216b
Laibarao2004 <laibarao393@gmail.com>
2025-11-02 09:45:48 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+// Buttons select karo
+const showProjectsBtn = document.getElementById("showProjectsBtn");
+const contactBtn = document.getElementById("contactBtn");
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+// Projects section select karo
+const projectsSection = document.querySelector(".projects");
+
+// Button 1: Projects show/hide
+showProjectsBtn.addEventListener("click", () => {
+  if (projectsSection.style.display === "none") {
+    projectsSection.style.display = "block";
+  } else {
+    projectsSection.style.display = "none";
+  }
+});
+
+// Button 2: Contact alert
+contactBtn.addEventListener("click", () => {
+  alert("You can contact me at laibarana908@gmail.com 💌");
+});
+
+// Button 3: Dark Mode toggle
+darkModeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+});
